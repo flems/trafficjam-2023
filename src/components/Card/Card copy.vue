0a512edc8c43b0625d8c3d922ae9d3d1f80570b9
Serg <borisov.s3@yandex.ru>
2023-05-06 20:26:18 +0300

    card v1

---
+++ src/components/Card/Card copy.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="card">
+    <div class="card__content">
+      123123132
+    </div>
+    <svg class="card__svg" viewBox="0 0 652 328" fill="none">
+      <g>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M21 282.011V45.3194C21 32.0646 31.7452 21.3195 45 21.3195H425.545C434.381 21.3195 441.545 28.4829 441.545 37.3194V50.0739C441.545 58.9105 448.708 66.0739 457.545 66.0739H614.341C623.177 66.0739 630.341 73.2374 630.341 82.0739V282.011C630.341 295.266 619.595 306.011 606.341 306.011H45C31.7452 306.011 21 295.266 21 282.011Z" fill="#6285FE"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M21 282.011V45.3194C21 32.0646 31.7452 21.3195 45 21.3195H425.545C434.381 21.3195 441.545 28.4829 441.545 37.3194V50.0739C441.545 58.9105 448.708 66.0739 457.545 66.0739H614.341C623.177 66.0739 630.341 73.2374 630.341 82.0739V282.011C630.341 295.266 619.595 306.011 606.341 306.011H45C31.7452 306.011 21 295.266 21 282.011Z" fill="#000210" fill-opacity="0.2"/>
+        <path d="M20 45.3194V282.011H22V45.3194H20ZM45 20.3195C31.1929 20.3195 20 31.5123 20 45.3194H22C22 32.6169 32.2975 22.3195 45 22.3195V20.3195ZM425.545 20.3195H45V22.3195H425.545V20.3195ZM442.545 50.0739V37.3194H440.545V50.0739H442.545ZM614.341 65.0739H457.545V67.0739H614.341V65.0739ZM631.341 282.011V82.0739H629.341V282.011H631.341ZM606.341 307.011C620.148 307.011 631.341 295.819 631.341 282.011H629.341C629.341 294.714 619.043 305.011 606.341 305.011V307.011ZM45 307.011H606.341V305.011H45V307.011ZM20 282.011C20 295.818 31.1929 307.011 45 307.011V305.011C32.2975 305.011 22 294.714 22 282.011H20ZM614.341 67.0739C622.625 67.0739 629.341 73.7897 629.341 82.0739H631.341C631.341 72.6851 623.729 65.0739 614.341 65.0739V67.0739ZM440.545 50.0739C440.545 59.4628 448.156 67.0739 457.545 67.0739V65.0739C449.261 65.0739 442.545 58.3582 442.545 50.0739H440.545ZM425.545 22.3195C433.829 22.3195 440.545 29.0352 440.545 37.3194H442.545C442.545 27.9306 434.934 20.3195 425.545 20.3195V22.3195Z" fill="#FCFDFD"/>
+      </g>
+      </svg>
+
+      <svg class="filter" viewBox="0 0 652 328">
+        <defs>
+          <filter id="cardFilter" x="0" y="0.319458" width="651.341" height="326.692" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="5.5"/>
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_117_1114"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="10"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.52 0 0 0 0 1 0 0 0 1 0"/>
+            <feBlend mode="normal" in2="effect1_backgroundBlur_117_1114" result="effect2_dropShadow_117_1114"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="10"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.52 0 0 0 0 1 0 0 0 1 0"/>
+            <feBlend mode="normal" in2="effect2_dropShadow_117_1114" result="effect3_dropShadow_117_1114"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="3.5"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0"/>
+            <feBlend mode="normal" in2="effect3_dropShadow_117_1114" result="effect4_dropShadow_117_1114"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect4_dropShadow_117_1114" result="shape"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="10"/>
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.88 0 0 0 0 1 0 0 0 1 0"/>
+            <feBlend mode="normal" in2="shape" result="effect5_innerShadow_117_1114"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="10"/>
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.88 0 0 0 0 1 0 0 0 1 0"/>
+            <feBlend mode="normal" in2="effect5_innerShadow_117_1114" result="effect6_innerShadow_117_1114"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="3.5"/>
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0"/>
+            <feBlend mode="normal" in2="effect6_innerShadow_117_1114" result="effect7_innerShadow_117_1114"/>
+          </filter>
+        </defs>
+      </svg>
+
+
+      <div class="clipped"></div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+// .svg {
+//   position: absolute;
+//   width: 0;
+//   height: 0;
+// }
+
+// .clipped {
+//   width: 100%;
+//   height: 350px;
+//   // background-size: cover;
+//   // -webkit-clip-path: url(#my-clip-path);
+//   clip-path: path('M0 320V24C0 10.7452 10.7452 0 24 0H189C201.15 0 211 9.84974 211 22V22C211 34.1503 220.85 44 233 44H376C389.255 44 400 54.7452 400 68V320C400 333.255 389.255 344 376 344H24C10.7452 344 0 333.255 0 320Z');
+// }
+
+svg {
+  width: 500px;
+  height: auto;
+}
+
+.card {
+  &__svg {
+    transition: all 0.2s;
+    
+    &:hover {
+      filter: url(#cardFilter);
+    }
+  }
+}
+
+// .card {
+//   // temp
+//   width: 400px;
+//   height: 350px;
+//   margin: 50px;
+//   position: relative;
+//   // outline: 1px solid #fff;
+
+//   // /temp
+
+//   &__content {
+//     height: 100%;
+//     width: 100%;
+//     padding-top: calc(44px + 20px);
+//     padding-left: 32px;
+//     padding-right: 32px;
+//   }
+
+//   &:before {
+//     content: '';
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     background: red;
+//     clip-path: path('M-2 24V320H2V24H-2ZM24 -2C9.64059 -2 -2 9.64059 -2 24H2C2 11.8497 11.8497 2 24 2V-2ZM189 -2H24V2H189V-2ZM376 42H233V46H376V42ZM402 320V68H398V320H402ZM376 346C390.359 346 402 334.359 402 320H398C398 332.15 388.15 342 376 342V346ZM24 346H376V342H24V346ZM-2 320C-2 334.359 9.64059 346 24 346V342C11.8497 342 2 332.15 2 320H-2ZM376 46C388.15 46 398 55.8497 398 68H402C402 53.6406 390.359 42 376 42V46ZM209 22C209 35.2548 219.745 46 233 46V42C221.954 42 213 33.0457 213 22H209ZM189 2C200.046 2 209 10.9543 209 22H213C213 8.74517 202.255 -2 189 -2V2Z');
+//     width: 100%;
+//     height: 100%;
+//   }
+
+//   // &:after {
+//   //   background: green;
+//   //   width: calc(100% - 4px);
+//   //   height: calc(100% - 6px);
+//   //   top: 2px;
+//   //   left: 2px;
+//   // }
+
+//   // border-radius: 0 0 24px 24px;
+//   // background: #000210;
+//   // border: 2px solid #6B6B6B;
+//   // border-top: none;
+//   // position: relative;
+//   // // box-shadow: inset 0 0 0 2px #6B6B6B;
+//   // padding-top: 44px;
+ 
+
+//   // &:before {
+//   //   content: '';
+//   //   width: calc(100% - 190px);
+//   //   height: 44px;
+//   //   display: block;
+//   //   position: absolute;
+//   //   bottom: 100%;
+//   //   left: -2px;
+//   //   border: 2px solid #6B6B6B;
+//   //   border-radius: 24px 24px 0 0;
+
+//   // }
+// }
+</style>
--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="card">
-    <div class="card__content">
-      123123132
+    <div class="card__top">
+      <span class="card__top-shadow"></span>
+      <span class="card__label-shadow"></span>
+      <span class="card__top-label">
+        <slot name="label" />
+      </span>
     </div>
-      <!-- <svg class="svg">
-        <clipPath id="my-clip-path" clipPathUnits="objectBoundingBox"><path d="M-0.005,0.069 V0.92 H0.005 V0.069 H-0.005 M0.059,-0.006 C0.024,-0.006,-0.005,0.028,-0.005,0.069 H0.005 C0.005,0.034,0.029,0.006,0.059,0.006 V-0.006 M0.468,-0.006 H0.059 V0.006 H0.468 V-0.006 M0.931,0.121 H0.577 V0.132 H0.931 V0.121 M0.995,0.92 V0.195 H0.985 V0.92 H0.995 M0.931,0.994 C0.966,0.994,0.995,0.961,0.995,0.92 H0.985 C0.985,0.954,0.961,0.983,0.931,0.983 V0.994 M0.059,0.994 H0.931 V0.983 H0.059 V0.994 M-0.005,0.92 C-0.005,0.961,0.024,0.994,0.059,0.994 V0.983 C0.029,0.983,0.005,0.954,0.005,0.92 H-0.005 M0.931,0.132 C0.961,0.132,0.985,0.16,0.985,0.195 H0.995 C0.995,0.154,0.966,0.121,0.931,0.121 V0.132 M0.517,0.063 C0.517,0.101,0.544,0.132,0.577,0.132 V0.121 C0.549,0.121,0.527,0.095,0.527,0.063 H0.517 M0.468,0.006 C0.495,0.006,0.517,0.031,0.517,0.063 H0.527 C0.527,0.025,0.501,-0.006,0.468,-0.006 V0.006"></path></clipPath>
-      </svg>
-
-      <div class="clipped"></div> -->
+    <span class="card__patch"></span>
+    <div class="card__content">
+      <slot name="content" />
+    </div>
   </div>
 </template>
 
@@ -15,80 +18,272 @@
 </script>
 
 <style lang="scss">
-.svg {
-  position: absolute;
-  width: 0;
-  height: 0;
-}
-
-.clipped {
-  width: 100%;
-  height: 350px;
-  // background-size: cover;
-  // -webkit-clip-path: url(#my-clip-path);
-  clip-path: path('M0 320V24C0 10.7452 10.7452 0 24 0H189C201.15 0 211 9.84974 211 22V22C211 34.1503 220.85 44 233 44H376C389.255 44 400 54.7452 400 68V320C400 333.255 389.255 344 376 344H24C10.7452 344 0 333.255 0 320Z');
-}
-
+$borderRadius: 20px;
+$labelWidth: 200px;
+$labelHeight: 44px;
+$borderWidth: 2px;
+$borderColor: #FCFDFD;
+$boxShadowLight: 0px 0px 7px #FFFFFF, 0px 0px 20px #00E0FF, 0px 0px 20px #00E0FF;
+$boxShadowDark: 0px 0px 7px #FFFFFF, 0px 0px 20px #0085FF, 0px 0px 20px #0085FF;
+$boxShadowLightInset: inset 0px 0px 7px #FFFFFF, inset 0px 0px 20px #00E0FF, inset 0px 0px 20px #00E0FF;
+$boxShadowHeight: 20px; 
+$cardBg: #6285FE;
+$transition: 0.2s ease-in;
 
 .card {
-  // temp
-  width: 400px;
-  height: 350px;
+  width: 640px;
+  height: 300px;
   margin: 50px;
+  display: flex;
+  flex-direction: column;
   position: relative;
-  // outline: 1px solid #fff;
+  min-width: $labelWidth + $borderRadius * 2;
+  cursor: pointer;
 
-  // /temp
+  // супер костыльная заплатка
+  &__patch {
+    &:before {
+      content: '';
+      width: $labelWidth;
+      height: 20px;
+      display: block;
+      position: absolute;
+      left: 2px;
+      top: 29px;
+      background: $cardBg;
+      border-radius: 9px;
+      pointer-events: none;
+      z-index: 4;
+    }
+    &:after {
+      content: '';
+      width: 20px;
+      height: 20px;
+      display: block;
+      position: absolute;
+      left: 191px;
+      top: 36px;
+      background: $cardBg;
+      border-radius: 50%;
+      pointer-events: none;
+      z-index: 4;
+    }
+  }
+  // /супер костыльная заплатка
 
-  &__content {
-    height: 100%;
-    width: 100%;
-    padding-top: calc(44px + 20px);
-    padding-left: 32px;
-    padding-right: 32px;
+  &__top {
+    height: $labelHeight + $boxShadowHeight;
+    margin-bottom: -($boxShadowHeight + $borderWidth);
+    position: relative;
+    z-index: 5;
+
+    &:after {
+      content: '';
+      position: absolute;
+      width: calc((100% - #{$labelWidth} - #{$borderWidth}) - $boxShadowHeight);
+      height: calc(#{$labelHeight}/2 - #{$borderWidth});
+      bottom: calc(#{$boxShadowHeight} + #{$borderWidth}/2);
+      left: $labelWidth - $borderWidth;
+      border: $borderWidth solid $borderColor;
+      border-bottom-left-radius: $borderRadius;
+      border-top: none;
+      border-right: none;
+      z-index: 1;
+      pointer-events: none;
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      width: calc((100% - #{$labelWidth} - #{$borderWidth})/2);
+      height: calc(#{$labelHeight}/2 - #{$borderWidth});
+      bottom: calc(#{$borderWidth}/2);
+      left: calc((100% - #{$labelWidth} - #{$borderWidth})/2 + #{$labelWidth});
+      border: $borderWidth solid $borderColor;
+      border-top-right-radius: $borderRadius;
+      border-bottom: none;
+      border-left: none;
+      z-index: 2;
+      pointer-events: none;
+    }
   }
 
-  &:before {
-    content: '';
+  &__top-label {
+    display: block;
+    width: $labelWidth;
+    height: $labelHeight + $boxShadowHeight;
+    position: relative;
+    border-left: $borderWidth solid $borderColor;
+    border-top-left-radius: $borderRadius;
+    border-top-right-radius: $borderRadius;
+    background: $cardBg;
+    padding: 18px 32px 0;
+    overflow: hidden;
+
+    &:before {
+      content: '';
+      position: absolute;
+      width: calc(100% - #{$borderWidth});
+      height: calc(#{$labelHeight}/2 - #{$borderWidth});
+      top: 0;
+      left: -$borderWidth;
+      border: $borderWidth solid $borderColor;
+      // background: green;
+      border-top-left-radius: $borderRadius;
+      border-top-right-radius: $borderRadius;
+      border-bottom: none;
+      z-index: 2;
+      pointer-events: none;
+      // border-left: none;
+    }
+  }
+
+  &__content {
+    flex-grow: 1;
+    border: $borderWidth solid $borderColor;
+    background: $cardBg;
+    border-top: none;
+    border-bottom-left-radius: $borderRadius;
+    border-bottom-right-radius: $borderRadius;
+    border-top-right-radius: $borderRadius;
+    position: relative;
+    overflow: hidden;
+    z-index: 3;
+    transition: box-shadow $transition;
+    padding: 20px 32px;
+
+    &:after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 200%;
+      bottom: 0;
+      left: 0;
+      transition: box-shadow $transition;
+    }
+  }
+}
+
+// inset shadow
+.card {
+  $parent: &;
+
+  &__top-shadow {
     position: absolute;
+    bottom: 0;
+    left: 0;
+    width: calc(100% - #{$boxShadowHeight}/2);
+    height: calc(100% - #{$boxShadowHeight}/2);
+    overflow: hidden;
+    pointer-events: none;
+    
+    &:after {
+      opacity: 0;
+      transition: opacity $transition;
+
+      content: '';
+      position: absolute;
+      width: 200%;
+      height: 200%;
+      bottom: calc(#{$boxShadowHeight} + #{$borderWidth}/2);
+      left: $labelWidth - $borderWidth;
+      border-bottom-left-radius: $borderRadius;
+      border-top: none;
+      border-right: none;
+      box-shadow: $boxShadowLight;
+      z-index: 1;
+    }
+  }
+
+  #{$parent}__top-label {
+    &:after {
+      content: '';
+      position: absolute;
+      top: $borderWidth;
+      border-radius: $borderRadius - 4px;
+      left: 0;
+      width: 200%;
+      height: 200%;
+      box-shadow: $boxShadowLightInset;
+      opacity: 0;
+      transition: opacity $transition;
+    }
+  }
+}
+
+// box-shadow
+.card {
+  $parent: &;
+
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 50px;
+    height: 100%;
+    z-index: -1;
+    background: red;
+    border-top-left-radius: $borderRadius;
+    border-bottom-left-radius: $borderRadius;
+    box-shadow: $boxShadowDark;
+    opacity: 0;
+    transition: opacity $transition;
+  }
+
+  &__label-shadow {
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    background: red;
-    clip-path: path('M-2 24V320H2V24H-2ZM24 -2C9.64059 -2 -2 9.64059 -2 24H2C2 11.8497 11.8497 2 24 2V-2ZM189 -2H24V2H189V-2ZM376 42H233V46H376V42ZM402 320V68H398V320H402ZM376 346C390.359 346 402 334.359 402 320H398C398 332.15 388.15 342 376 342V346ZM24 346H376V342H24V346ZM-2 320C-2 334.359 9.64059 346 24 346V342C11.8497 342 2 332.15 2 320H-2ZM376 46C388.15 46 398 55.8497 398 68H402C402 53.6406 390.359 42 376 42V46ZM209 22C209 35.2548 219.745 46 233 46V42C221.954 42 213 33.0457 213 22H209ZM189 2C200.046 2 209 10.9543 209 22H213C213 8.74517 202.255 -2 189 -2V2Z');
-    width: 100%;
-    height: 100%;
+    position: absolute;
+    z-index: -1;
+
+    &:before {
+      content: '';
+      display: block;
+      position: absolute;
+      border-top-left-radius: $borderRadius;
+      border-top-right-radius: $borderRadius;
+      top: 0;
+      left: 0;
+      width: $labelWidth;
+      height: 30px;
+      background: red;
+      box-shadow: $boxShadowDark;
+      opacity: 0;
+      transition: opacity $transition;
+    }
   }
+}
 
-  // &:after {
-  //   background: green;
-  //   width: calc(100% - 4px);
-  //   height: calc(100% - 6px);
-  //   top: 2px;
-  //   left: 2px;
-  // }
+// hover
+.card {
+  $parent: &;
 
-  // border-radius: 0 0 24px 24px;
-  // background: #000210;
-  // border: 2px solid #6B6B6B;
-  // border-top: none;
-  // position: relative;
-  // // box-shadow: inset 0 0 0 2px #6B6B6B;
-  // padding-top: 44px;
- 
+  &:hover {
+    #{$parent}__top-shadow:after {
+      opacity: 1;
+    }
 
-  // &:before {
-  //   content: '';
-  //   width: calc(100% - 190px);
-  //   height: 44px;
-  //   display: block;
-  //   position: absolute;
-  //   bottom: 100%;
-  //   left: -2px;
-  //   border: 2px solid #6B6B6B;
-  //   border-radius: 24px 24px 0 0;
+    &:after {
+      opacity: 1;
+    }
 
-  // }
+    #{$parent}__label-shadow:before {
+      opacity: 1;
+    }
+
+    #{$parent}__content:after {
+      box-shadow: $boxShadowLightInset;
+    }
+
+    #{$parent}__content {
+      box-shadow: $boxShadowDark;
+    }
+
+    #{$parent}__top-label:after {
+      opacity: 1;
+    }
+  }
 }
 </style>
